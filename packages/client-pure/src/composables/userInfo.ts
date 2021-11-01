87866e9dd73ce564df14713597d4f91c3f24c946
Mr.Hope <zhangbowang1998@gmail.com>
2021-11-01 16:00:41 +0800

    feat: add client-pure

---
+++ packages/client-pure/src/composables/userInfo.ts
@@ -0,0 +1,41 @@
+import { readonly, ref } from 'vue';
+import type { Ref } from 'vue';
+
+export interface UserInfo {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  display_name: string;
+  email: string;
+  url: string;
+  token: string;
+  avatar: string;
+  mailMd5: string;
+}
+
+export type UserInfoRef = Ref<UserInfo | Record<string, never>>;
+
+const USER_KEY = 'WALINE_USER';
+
+const userInfo: UserInfoRef = ref({});
+
+export const useUserInfo = (): {
+  userInfo: UserInfoRef;
+  setUserInfo: (userInfo: UserInfo | Record<string, never>) => void;
+} => {
+  if (!userInfo.value)
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      userInfo.value =
+        JSON.parse(localStorage.getItem(USER_KEY) || '') ||
+        JSON.parse(sessionStorage.getItem(USER_KEY) || '') ||
+        {};
+    } catch (err) {
+      // do nothing
+    }
+
+  return {
+    userInfo: readonly(userInfo),
+    setUserInfo: (info: UserInfo | Record<string, never>): void => {
+      userInfo.value = info;
+    },
+  };
+};
